ece08cb976943686098b5274efa3059a88a45668
Nozhan Amiri <nozhana@gmail.com>
2024-04-04 16:38:45 +0330

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Nozhan Amiri on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
